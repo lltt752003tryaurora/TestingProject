7958b9ea3622a0802d821414de39086279acfb0a
Le Long Truong Thinh <truongthinhkl1@gmail.com>
2024-07-13 16:12:35 +0700

    create issues

--- controllers/api/issueController.js
+++ controllers/api/issueController.js
@@ -9,7 +9,7 @@ const controller = {
             const page = isNaN(req.query.page) ? 1 : Math.max(1, parseInt(req.query.page));
             const sortField = req.query.sort === 'updatedAt' ? 'updatedAt' : 'id';
             const sortOrder = req.query.order === 'asc' ? 'ASC' : 'DESC';
-    
+
             const options = {
                 where: {},
                 offset: PAGE_LIMIT * (page - 1),
@@ -26,19 +26,19 @@ const controller = {
                     }]
                 }]
             };
-    
+
             const keyword = req.query.keyword || '';
             if (keyword.trim() !== '') {
                 options.where.name = { [Op.iLike]: `%${keyword}%` };
             }
-    
+
             try {
                 const issues = await db.Issue.findAll(options);
                 const issueCount = await db.Issue.count({
                     where: options.where,
                     include: options.include
                 });
-    
+
                 res.send({
                     page: page,
                     totalPages: Math.ceil(issueCount / PAGE_LIMIT),
@@ -56,9 +56,40 @@ const controller = {
     createIssue: [
         async (req, res, next) => {
             try {
+                const userId = req.user.id;
+                const projectId = req.project.id;
 
-            } catch (err) {
+                let { name, description, priority, status, assignedUserId, testRunId } = req.body;
 
+                if (!testRunId) {
+                    return res.status(400).send({
+                        message: 'Missing test run ID.'
+                    });
+                }
+
+                const newIssue = await db.Issue.create({
+                    name,
+                    description,
+                    priority,
+                    status,
+                    assignedUserId,
+                    creatorUserId: userId,
+                    testRunId
+                });
+
+                activityHelper.createActivity(projectId, userId, 'CreateIssue', JSON.stringify({
+                    issueId: newIssue.id,
+                }));
+
+                res.status(200).send({
+                    message: "Successfully created issue"
+                });
+
+            } catch (error) {
+                console.error('Error while creating issue:', error);
+                res.status(500).send({
+                    message: 'Internal server error.'
+                });
             }
         }
     ],

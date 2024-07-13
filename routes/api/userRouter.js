b9c91a5766fc28c882a4726001bc271fa7b664c1
EzioNoir <ezionoir@outlook.com>
2024-07-13 19:10:31 +0700

    merge

--- routes/api/userRouter.js
+++ routes/api/userRouter.js
@@ -3,10 +3,6 @@ const router = express.Router({mergeParams: true});
 const userController = require('../../controllers/api/userController');
 const {roleWhitelist} = require("../../middlewares/roleMiddleware")
 
-<<<<<<< HEAD
-router.get('/:userId', userController.getUserById);
-router.get('/', userController.getUsersByUsername);
-=======
 router.get('/', userController.getUserSelf);
 router.put('/', userController.editUserSelf);
 router.delete('/', userController.deleteUserSelf);
@@ -15,6 +11,5 @@ router.get('/:userId', roleWhitelist(['admin']), userController.getUserById);
 router.post('/', roleWhitelist(['admin']), userController.createUser);
 router.put('/:userId', roleWhitelist(['admin']), userController.editUser);
 router.delete('/:userId', roleWhitelist(['admin']), userController.deleteUser);
->>>>>>> refs/remotes/origin/release-3.0
 
 module.exports = router;
